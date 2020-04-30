c63824bd5258475eb57a90e36da8abe42b660fd6
Joao Paulo Soubihe <j151106@students.ic.unicamp.br>
2020-04-30 11:54:53 -0300

    best first search + some measures for analysis

--- PacmanProblem.py
+++ PacmanProblem.py
@@ -2,10 +2,12 @@ import readMaze as rM
 from search import Node, Problem
 import math
 import functools
-from search import Node
-from best_first_graph_search_for_vis import best_first_graph_search_for_vis, astar_search_graph
+from search import Node, breadth_first_tree_search
+from best_first_graph_search_for_vis import best_first_graph_search_for_vis, astar_search_graph, greedy_best_first_search, uniform_cost_search
 from notebook import show_map, display_visual
 from search import UndirectedGraph
+import time as time
+
 
 class PacmanProblem(Problem):
     def __init__(self, initial, goal, maze=None):
@@ -15,53 +17,53 @@ class PacmanProblem(Problem):
             self.maze, self.mazeY, self.mazeX = rM.readMaze()
         else:
             self.maze, self.mazeY, self.mazeX = rM.readMaze(maze)
-    
+
     # for a given position returns the adjacent positions that are gray paths
     def adjacent(self, position):
         adjacent = []
         # left
-        if self.maze[position[0], position[1]-1] == 2:
-            if position[1] -1 < 0:
-                adjacent.append((position[0], self.mazeX -1))
+        if self.maze[position[0], position[1] - 1] == 2:
+            if position[1] - 1 < 0:
+                adjacent.append((position[0], self.mazeX - 1))
             else:
-                adjacent.append((position[0], position[1]-1))
+                adjacent.append((position[0], position[1] - 1))
         # right
         if position[1] + 1 == self.mazeX and self.maze[position[0], 0] == 2:
             adjacent.append((position[0], 0))
-        elif position[1] + 1 < self.mazeX and self.maze[position[0], position[1]+1] == 2:
-            adjacent.append((position[0], position[1]+1))
-                
+        elif position[1] + 1 < self.mazeX and self.maze[position[0], position[1] + 1] == 2:
+            adjacent.append((position[0], position[1] + 1))
+
         # up
-        if self.maze[position[0]-1, position[1]] == 2:
-            if position[0] -1 < 0:
-                adjacent.append((self.mazeY-1, position[1]))
+        if self.maze[position[0] - 1, position[1]] == 2:
+            if position[0] - 1 < 0:
+                adjacent.append((self.mazeY - 1, position[1]))
             else:
-                adjacent.append((position[0]-1, position[1]))
+                adjacent.append((position[0] - 1, position[1]))
         # down
         if position[0] + 1 == self.mazeY and self.maze[0, position[1]] == 2:
             adjacent.append((0, position[1]))
-        elif position[0] + 1 < self.mazeY and self.maze[position[0]+1, position[1]] == 2:
-            adjacent.append((position[0]+1, position[1]))
-        
+        elif position[0] + 1 < self.mazeY and self.maze[position[0] + 1, position[1]] == 2:
+            adjacent.append((position[0] + 1, position[1]))
+
         return adjacent
 
-     # returns the possible child states for the given state
+    # returns the possible child states for the given state
     def actions(self, state):
         return self.adjacent(state)
 
-    # the goal is reached if all positions that are reachable through the initial state are in the solution 
+    # the goal is reached if all positions that are reachable through the initial state are in the solution
     # and the first and last positions in the solution are the initial position and goal position
-    def goal_test(self, states):
+    def goal_test(self, states):    
         return states[-1] == self.goal and states[0] == self.initial
-    
+
     def result(self, state, action):
         return action
-    
+
     def path_cost(self, c, position1, action, position2):
         # Se as posicoes n forem adjacentes entao nao ha um caminho direto de pos1 pra pos2
         if abs(position1[0] - position2[0]) > 1 or abs(position1[1] - position2[1]) > 1:
             return math.inf
-        return c+1
+        return c + 1
 
     # Retorna uma lista com todas as posicoes alcancaveis a partir da posicao dada, inclusive essa posicao
     def reachable_positions(self, startPosition):
@@ -89,7 +91,6 @@ class PacmanProblem(Problem):
     def reachable(self, position1, position2):
         positions = self.reachable_positions(position1)
         return position2 in positions
-
 
     # Cada posicao andada custa 1, porem cada ponto unico recolhido vale 2
     def value(self, states):
@@ -124,13 +125,13 @@ class PacmanProblem(Problem):
 
             neighbors = self.adjacent(current)
             for neighbor in neighbors:
-                if d[neighbor][1] == 0 and m+1 < d[neighbor][0]:
-                    d[neighbor] = [m+1, 0]
+                if d[neighbor][1] == 0 and m + 1 < d[neighbor][0]:
+                    d[neighbor] = [m + 1, 0]
                     if neighbor == position2:
-                        return m+1
+                        return m + 1
 
             current = None
-            i = i+1
+            i = i + 1
 
         try:
             return d[position2][0]
@@ -160,15 +161,15 @@ class PacmanProblem(Problem):
 
             neighbors = self.adjacent(current)
             for neighbor in neighbors:
-                if d[neighbor][1] == 0 and m+1 < d[neighbor][0]:
+                if d[neighbor][1] == 0 and m + 1 < d[neighbor][0]:
                     pathCopy = path.copy()
                     pathCopy.append(neighbor)
-                    d[neighbor] = [m+1, 0, pathCopy]
+                    d[neighbor] = [m + 1, 0, pathCopy]
                     if neighbor == position2:
                         return pathCopy
 
             current = None
-            i = i+1
+            i = i + 1
 
         try:
             pathCopy
@@ -176,7 +177,9 @@ class PacmanProblem(Problem):
             return []
 
 
-pacman_problem = PacmanProblem((1,1), (8,11))
+# best_first_graph_search_for_vis(PacmanProblem((1,1), (8,11), lambda node : node.path_cost))
+
+pacman_problem = PacmanProblem((1, 1), (8, 11))
 
 reachable = pacman_problem.reachable_positions(pacman_problem.initial)
 
@@ -204,15 +207,33 @@ pacman_map.locations = aux
 
 node_colors = {node: 'white' for node in pacman_map.locations.keys()}
 node_positions = pacman_map.locations
-node_label_pos = { k:[v[0]-0.25, v[1]-0.4] for k,v in pacman_map.locations.items() }
-edge_weights = { (k,k2) : 1 for k,v in pacman_map.graph_dict.items() for k2,v2 in v.items()}
+node_label_pos = {k: [v[0] - 0.25, v[1] - 0.4] for k, v in pacman_map.locations.items()}
+edge_weights = {(k, k2): 1 for k, v in pacman_map.graph_dict.items() for k2, v2 in v.items()}
 
+start = time.time()
 iterations, all_node_colors, node = astar_search_graph(pacman_problem)
+end = time.time()
+print("time elapsed for astar_search_graph = "+ str(end - start) + 's')
+print("iterations = "+ str(iterations))
+
+start = time.time()
+iterations, all_node_colors, node = greedy_best_first_search(pacman_problem)
+end = time.time()
+print("time elapsed for greedy_best_first_search = "+ str(end - start) + 's')
+print("iterations = "+ str(iterations)),
+
+start = time.time()
+iterations, all_node_colors, node = uniform_cost_search(pacman_problem)
+end = time.time()
+print("time elapsed for uniform_cost_search = "+ str(end - start) + 's')
+print("iterations = "+ str(iterations))
+
+# print(uniform_cost_search(pacman_problem).solution)
 
 result_node_colors = all_node_colors[-1]
 
 node_colors = {}
-for k,v in result_node_colors.items():
+for k, v in result_node_colors.items():
     for ki, vi in aux.items():
         if aux[ki] == k:
             node_colors[ki] = v
@@ -226,4 +247,7 @@ pacman_graph_data = {
     'edge_weights': edge_weights
 }
 
-show_map(pacman_graph_data)
+# show_map(pacman_graph_data)
+
+# breadth_first_tree_search(PacmanProblem((1, 1), (8, 11)))
+# display_visual(pacman_graph_data, True, best_first_graph_search_for_vis, PacmanProblem((1,1), (8,11)))
